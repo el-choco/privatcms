e222675675ffe6670e4ee5ae2d529ed91564503d
el-choco <f.sanchez@unity-mail.de>
2026-01-14 15:28:30 +0100

    force some files

--- admin/settings.php
+++ admin/settings.php
@@ -122,7 +122,7 @@ require_once 'header.php';
 </style>
 
 <div class="admin-content">
-    <div style="max-width: 1500px; margin: 0 auto;">
+    <div style="max-width: 1530px; margin: 0 auto;">
         <h1 style="margin-bottom: 25px;"><?= htmlspecialchars($sLang['header'] ?? 'System Settings') ?></h1>
         <?= $message ?>
 
